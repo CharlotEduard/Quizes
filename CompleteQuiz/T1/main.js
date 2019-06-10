d6077327b2408e63e46ecd3af50f0f8677d9bba8
enryson <enryson@gmail.com>
2019-06-10 18:03:18 -0300

    up

--- CompleteQuiz/T1/main.js
+++ CompleteQuiz/T1/main.js
@@ -39,7 +39,7 @@ function quit() {
     localStorage.setItem("T1", (f * 100));
 
     //redireciona para a pagina home
-    window.location.replace("Quizes/index.html");
+    window.location.replace("../../index.html");
 }
 
 function give_ques(quesindex) {
